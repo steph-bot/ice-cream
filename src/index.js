9eb14e8a51a5c5102fb66bab4ba7a7c7510d073b
Stephanie Mathew <stephanie.mathew@liongard.com>
2020-07-30 20:53:13 -0500

    Logic in progress.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const random = require("random");
 
+// STEPH FIX TIMEWINDOWHOURS = 7 PER PROBLEM STATMENT
 const timeWindowHours = 7; // How many hours until VIP Customer shows up
 const timeWindowMins = timeWindowHours * 60;
-debugger;
 
 const coneTimeStats = {
     mean: 7, // (minutes) avg to create single cone
     stdDev: 1 // (minutes) std deviation
 };
 
+// STEPH REMOVE MATH.ROUND!
 const calculateConeTime = random.normal(coneTimeStats.mean, coneTimeStats.stdDev);
 
 const customerArrivalStats = {
@@ -17,44 +18,68 @@ const customerArrivalStats = {
 
 const lambda = 1 / customerArrivalStats.mean;
 
+// STEPH REMOVE MATH.ROUND!
 const calcTimeBetweenCustomers = random.exponential(lambda);
 
 const simulationIterations = 1001;
-/*
-console.log(calculateConeTime());
-console.log(calculateConeTime());
-console.log(calculateConeTime());
-
-debugger;
-
-console.log(calcTimeBetweenCustomers());
-console.log(calcTimeBetweenCustomers());
-console.log(calcTimeBetweenCustomers());
-*/
 
 let currentTimeMins = 0;
+const customerQueue = [];
 
-if (currentTimeMins < timeWindowMins) {
-  const customerQueue = [];
+const customerArrives = (arrivalTime) => {
+  const cone = Math.round(calculateConeTime()); // REMOVE MATH ROUND STEPH!!!!!
+  const customer = {
+    arrivalTime: arrivalTime,
+    coneTime: cone,
+    earliestLeaveTime: arrivalTime + cone
+  };
+  customerQueue.push(customer);
+}
 
-  const customerArrives = (currentTime) => {
-      const customer = {
-          arrivalTime: currentTime + calcTimeBetweenCustomers(),
-          coneTime: calculateConeTime()
-      };
+for (let time = 0; time < timeWindowMins;) {
+  console.log('Start time: ' + time);
 
-    }
+  const timeTillNextCustomer = Math.round(calcTimeBetweenCustomers()); // REMOVE MATH ROUND STEPH!!!!!
 
-  customerArrives(currentTimeMins);
+  console.log('Time till next cust: ' + timeTillNextCustomer)
 
-    // Customer arrives
-    // Customer orders cone
-    const timeTillConeComplete = calculateConeTime();
-    // Cone is complete
-    currentTimeMins = currentTimeMins + timeTillConeComplete;
+  const arrivalTime = time + timeTillNextCustomer;
+
+  if (arrivalTime < timeWindowMins) {
+    time = arrivalTime;
+    customerArrives(arrivalTime);
+  } else {
+    time = timeWindowMins;
+  }
+
+  console.log('Current time: ' + time);
+  console.log(customerQueue)
+}
+
+
+debugger; //STEPH STPHE TPHSEPAHSOIJDA;SDJ;AJS;I
+for (let i = 0; i < (customerQueue.length-1); i++) {
+  //
+  customerQueue[i].leaveTime = (customerQueue[i].waitTime || 0) + customerQueue[i].earliestLeaveTime;
+  customerQueue[i + 1].waitTime = customerQueue[i].leaveTime - customerQueue[i + 1].arrivalTime;
+
+  if (customerQueue[i + 1].waitTime < 0) {
+    customerQueue[i + 1].waitTime = 0;
+  }
+
+  console.log(customerQueue);
+  // debugger;
 
 
 }
+
+
+
+  // Customer arrives
+  // Customer orders cone
+  // Cone is complete
+
+
 
 /*
 Assumptions:
