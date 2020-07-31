0d858a24581c616ffb36609ae1faa9c2686fb01d
Stephanie Mathew <stephanie.mathew@liongard.com>
2020-07-30 22:43:05 -0500

    Run simulation 1001 times

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const random = require("random");
 
-// STEPH FIX TIMEWINDOWHOURS = 7 PER PROBLEM STATMENT
+/*
+
++ Assumptions +
+
+- Can only make one cone at a time
+- Customers must wait in line after arriving if a cone is in progress
+
+*/
+
+// STEPH FIX timeWindowHours = 7 PER PROBLEM STATMENT
 const timeWindowHours = 7; // How many hours until VIP Customer shows up
 const timeWindowMins = timeWindowHours * 60;
 
@@ -9,99 +18,88 @@ const coneTimeStats = {
     stdDev: 1 // (minutes) std deviation
 };
 
-// STEPH REMOVE MATH.ROUND!
 const calculateConeTime = random.normal(coneTimeStats.mean, coneTimeStats.stdDev);
 
 const customerArrivalStats = {
     mean: 7, // (minutes) avg time between customer arrivals
 };
 
-const lambda = 1 / customerArrivalStats.mean;
+const customerLambda = 1 / customerArrivalStats.mean;
 
-// STEPH REMOVE MATH.ROUND!
-const calcTimeBetweenCustomers = random.exponential(lambda);
+const calcTimeBetweenCustomers = random.exponential(customerLambda);
 
 const simulationIterations = 1001;
+const waitTimesArray = [];
 
-let currentTimeMins = 0;
-const customerQueue = [];
+for (let i = 0; i < simulationIterations; i++) {
 
-const customerArrives = (arrivalTime) => {
-  const cone = Math.round(calculateConeTime()); // REMOVE MATH ROUND STEPH!!!!!
-  const customer = {
-    arrivalTime: arrivalTime,
-    coneTime: cone,
-    earliestLeaveTime: arrivalTime + cone
-  };
-  customerQueue.push(customer);
-}
-
-for (let time = 0; time < timeWindowMins;) {
-  console.log('Start time: ' + time);
-
-  const timeTillNextCustomer = Math.round(calcTimeBetweenCustomers()); // REMOVE MATH ROUND STEPH!!!!!
-
-  console.log('Time till next cust: ' + timeTillNextCustomer)
-
-  const arrivalTime = time + timeTillNextCustomer;
-
-  if (arrivalTime < timeWindowMins) {
-    time = arrivalTime;
-    customerArrives(arrivalTime);
-  } else {
-    time = timeWindowMins;
-  }
-
-  console.log('Current time: ' + time);
-  // console.log(customerQueue)
-}
-
-console.log(customerQueue);
-
-
-debugger; //STEPH STPHE TPHSEPAHSOIJDA;SDJ;AJS;I
-
-customerQueue[0].leaveTime = customerQueue[0].earliestLeaveTime;
-
-for (let i = 1; i < (customerQueue.length); i++) {
-  // customerQueue[i].leaveTime = (customerQueue[i].waitTime || 0) + customerQueue[i].earliestLeaveTime;
-  // customerQueue[i + 1].waitTime = customerQueue[i].leaveTime - customerQueue[i + 1].arrivalTime;
-
-  // Refactor
-  customerQueue[i].waitTime = customerQueue[i-1].leaveTime - customerQueue[i].arrivalTime;
-  if (customerQueue[i].waitTime < 0) {
-    customerQueue[i].waitTime = 0;
-  }
-  customerQueue[i].leaveTime = customerQueue[i].waitTime + customerQueue[i].earliestLeaveTime;
-
-
-  
-
-  // console.log(customerQueue);
   // debugger;
 
+  const customerQueue = [];
 
+  const customerArrives = (arrivalTime) => {
+    const cone = calculateConeTime(); // REMOVE Math.round() STEPH!!!!!
+    const customer = {
+      arrivalTime: arrivalTime,
+      coneTime: cone,
+      earliestLeaveTime: arrivalTime + cone
+    };
+    customerQueue.push(customer);
+  }
+
+  for (let time = 0; time < timeWindowMins;) {
+    const timeTillNextCustomer = calcTimeBetweenCustomers(); // REMOVE Math.round() STEPH!!!!!
+    const arrivalTime = time + timeTillNextCustomer;
+
+    if (arrivalTime < timeWindowMins) {
+      time = arrivalTime;
+      customerArrives(arrivalTime);
+    } else {
+      time = timeWindowMins;
+    }
+  }
+
+  // console.log(customerQueue);
+  // debugger; // STEPH
+
+  customerQueue[0].leaveTime = customerQueue[0].earliestLeaveTime;
+
+  for (let i = 1; i < (customerQueue.length); i++) {
+    customerQueue[i].waitTime = customerQueue[i-1].leaveTime - customerQueue[i].arrivalTime;
+    if (customerQueue[i].waitTime < 0) {
+      customerQueue[i].waitTime = 0;
+    }
+    customerQueue[i].leaveTime = customerQueue[i].waitTime + customerQueue[i].earliestLeaveTime;
+  }
+
+  const VIPCustomer = {
+    arrivalTime: timeWindowMins
+  };
+  VIPCustomer.waitTime = customerQueue[customerQueue.length - 1].leaveTime - VIPCustomer.arrivalTime;
+  if (VIPCustomer.waitTime < 0) {
+    VIPCustomer.waitTime = 0;
+  }
+
+  // console.log(customerQueue);
+  // console.log('');
+  // console.log(customerQueue[customerQueue.length - 1])
+  // console.log('');
+  console.log('i: ' + i + ' Wait Time: ' + VIPCustomer.waitTime);
+  waitTimesArray.push(VIPCustomer.waitTime);
+  // debugger; // STEPH
+
+
+  /*
+  let average = (array) => array.reduce((a, b) => a + b) / array.length;
+  console.log(average([1,2,3,4]));
+  */
 }
-console.log(customerQueue);
+
+console.log(waitTimesArray);
 
 
-  // Customer arrives
-  // Customer orders cone
-  // Cone is complete
+const arrayAverage = (array) => array.reduce((a, b) => a + b) / array.length;
 
-
-
-/*
-Assumptions:
-- Can only make one cone at a time
-- Customers must wait in line after arriving if a cone is in progress
-*/
-
+console.log('Average: ' + arrayAverage(waitTimesArray));
 
 debugger;
-
-
-/*
-let average = (array) => array.reduce((a, b) => a + b) / array.length;
-console.log(average([1,2,3,4]));
-*/
