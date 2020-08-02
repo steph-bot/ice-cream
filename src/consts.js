1050612ebbee84fa2f87c46d8f4edcbcad1b2a25
Stephanie Mathew <stephanie.mathew@liongard.com>
2020-08-02 00:34:39 -0500

    Linting

--- src/consts.js
+++ src/consts.js
@@ -1,21 +1,21 @@
-const random = require("random");
+const random = require('random');
 
 const input = {
-    timeWindow: 7, // hours until VIP customer arrives
-    coneTimeMeanMins: 7,   // avg time to create single cone
-    coneTimeStdDevMins: 1,  // std deviation
-    customerArrivalMeanMins: 7, // avg time b/w customer arrivals
-    simulationRuns: 1001 // how many simulation iterations
+  timeWindow: 7, // hours until VIP customer arrives
+  coneTimeMeanMins: 7, // avg time to create single cone
+  coneTimeStdDevMins: 1, // std deviation
+  customerArrivalMeanMins: 7, // avg time b/w customer arrivals
+  simulationRuns: 1001, // how many simulation iterations
 
-}
+};
 
 const timeWindow_i = {};
 timeWindow_i.hours = input.timeWindow;
 timeWindow_i.mins = timeWindow_i.hours * 60;
 
 const coneTimeStats = {
-    mean: input.coneTimeMeanMins,
-    stdDev: input.coneTimeStdDevMins
+  mean: input.coneTimeMeanMins,
+  stdDev: input.coneTimeStdDevMins,
 };
 
 const calculateConeTime_i = random.normal(coneTimeStats.mean, coneTimeStats.stdDev);
@@ -29,8 +29,8 @@ const calcTimeBetweenCustomers_i = random.exponential(customerArrivalStats.lambd
 const simulationRuns_i = input.simulationRuns;
 
 module.exports = {
-    timeWindow_i,
-    calculateConeTime_i,
-    calcTimeBetweenCustomers_i,
-    simulationRuns_i
-}
+  timeWindow_i,
+  calculateConeTime_i,
+  calcTimeBetweenCustomers_i,
+  simulationRuns_i,
+};
